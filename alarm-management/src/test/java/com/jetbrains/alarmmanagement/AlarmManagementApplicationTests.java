b87ebfb27501edf48b44b60a363e4fa9f85c36d9
Marco Behler <marco@marcobehler.com>
2022-02-08 14:35:14 +0100

    first commit

---
+++ alarm-management/src/test/java/com/jetbrains/alarmmanagement/AlarmManagementApplicationTests.java
@@ -0,0 +1,42 @@
+package com.jetbrains.alarmmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+class AlarmManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1,1);
+	}
+
+	@Test
+	void contextLoads_1() {
+		assertEquals(2,2);
+	}
+
+	@Test
+	void contextLoads_2() {
+		assertEquals(2,2);
+	}
+
+	@Test
+	void contextLoads_3() {
+		assertEquals(2,2);
+	}
+
+	@Test
+	void contextLoads_4() {
+		assertEquals(2,2);
+	}
+
+
+	@Test
+	void contextLoads_5() {
+		assertEquals(2,2);
+	}
+
+}
---
+++ alarm-management/src/test/java/com/jetbrains/alarmmanagement/SecondTests.java
@@ -0,0 +1,45 @@
+
+package com.jetbrains.alarmmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@SpringBootTest
+class SecondTests {
+
+	@Test
+	void secondContextLoads() {
+		assertEquals(1,1);
+	}
+
+	@Test
+	void secondContextLoads_1() {
+		assertEquals(2,2);
+	}
+
+	@Test
+	void secondContextLoads_2() {
+		assertEquals(2,2);
+	}
+
+	@Test
+	void secondContextLoads_3() {
+		assertEquals(2,2);
+	}
+
+	@Test
+	void secondContextLoads_4() {
+		assertEquals(2,2);
+	}
+
+	@Test
+	void secondContextLoads_5() {
+		if (System.currentTimeMillis() % 2 == 0) {
+			fail("Flaky test");
+
+		}
+	}
+}
