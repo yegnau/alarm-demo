5e0ddf4135fa578864827c5fcbfb38892e4db843
Marco Behler <marco@marcobehler.com>
2022-02-09 13:07:40 +0100

    added new frontend and deployment instructions

---
+++ alarm-frontend/src/App.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from '@cypress/react';
+import App from './App';
+
+it('renders learn react link', () => {
+  mount(<App />);
+  cy.get('a').contains('Learn React');
+});
+
+it('renders  another learn react link', () => {
+  mount(<App />);
+  cy.get('a').contains('Learn React');
+});
+
+it('renders  even more another learn react link', () => {
+  mount(<App />);
+  cy.get('a').contains('Learn React');
+});
